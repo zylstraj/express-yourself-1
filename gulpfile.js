c69c0637bfb5ffcfac002b1a0fa9253c1839db72
Sarah Paetsch <spaetsch@gmail.com>
2015-07-21 10:52:10 -0700

    Gulpfile works!

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-use strict;
+'use strict';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
@@ -9,28 +9,22 @@ var uglify = require('gulp-uglify');
 gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./build/css/'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('compress', function() {
-  return gulp.src('lib/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
-});
-
 gulp.task('webpackdev', function() {
-  return gulp.src('app/js/**/*.js')
+  return gulp.src('./dev/js/main.js')
     .pipe(webpack({
       output: {
         filename: 'bundle.js'
       }
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('public/'));
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('minify-css', function() {
@@ -39,11 +33,22 @@ gulp.task('minify-css', function() {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('compress', function() {
+  return gulp.src('lib/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('copy', function() {
   var opts = {
     conditionals: true,
     spare:true
   };
+    return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dev/'))
+    //.pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./dev/'));
+});
 
 gulp.task('build', ['copy', 'webpackdev', 'sass']);
 gulp.task('default', ['build']);
