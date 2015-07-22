8afe2f4d39add8d0d9fc0c465f637509571d60ea
Josh Zylstra <zylstrajosh@gmail.com>
2015-07-22 11:01:24 -0700

    okay

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var bodyParser = require('body-parser');
-var Setting    = require('../models/donut-model.js');
+var Donut = require('../models/donut-model.js');
 
 module.exports = function(router) {
   router.use(bodyParser.json());
   router.get('/donuts', function(req, res) {
     console.log('Index router');
-    Setting.find({}, function(err, data) {
+    Donut.find({}, function(err, data) {
       if (err) {
         console.log(err);
       }
@@ -33,7 +33,7 @@ module.exports = function(router) {
     var updatedSetting = req.body;
     delete updatedSetting._id;
 
-    Setting.update({'_id': req.params.id}, updatedSetting, function (err, data) {
+    Donut.update({'_id': req.params.id}, updatedSetting, function (err, data) {
       console.log(req.body);
       if (err) {
         errorResponse(err, res);
@@ -45,7 +45,7 @@ module.exports = function(router) {
 
   router.delete('/donuts/:id', function(req, res) {
     console.log('You hit delete');
-    Setting.remove({'_id': req.params.id}, function(err, data) {
+    Donut.remove({'_id': req.params.id}, function(err, data) {
       if (err) {
         errorResponse(err, res);
         return;
