58ce223699d60cb35492697c320deb59da3d35cb
Josh Zylstra <zylstrajosh@gmail.com>
2015-07-22 12:05:19 -0700

    merge conflicts

--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,16 @@
 'use strict';
 
 var bodyParser = require('body-parser');
-<<<<<<< HEAD
 var Donut = require('../models/donut-model.js');
-=======
-var Donut    = require('../models/donut-model.js');
-
 console.log("in routes js");
->>>>>>> 4d26517482631363f8e751b9ff29b70dfd987193
 
 module.exports = function(router) {
   console.log("in module exports routes js");
   router.use(bodyParser.json());
   router.get('/donuts', function(req, res) {
-<<<<<<< HEAD
     console.log('Index router');
-=======
     console.log('hit the get');
->>>>>>> 4d26517482631363f8e751b9ff29b70dfd987193
+
     Donut.find({}, function(err, data) {
       if (err) {
         console.log(err);
@@ -28,11 +21,7 @@ module.exports = function(router) {
 
   router.post('/donuts', function(req, res) {
     console.log('You hit the post')
-<<<<<<< HEAD
-    var newSetting = new Donut(req.body);
-=======
     var newDonut = new Donut(req.body);
->>>>>>> 4d26517482631363f8e751b9ff29b70dfd987193
     console.log(req.body);
     newDonut.save({}, function(err, data) {
       if (err){
